569d9bda9b9e35433fceeb371151421d17d8bed4
GollapudiLavanya <lavanyagollapudi5166@gmail.com>
2021-07-15 10:55:46 +0530

    [LAVANYA]Initial Commit

---
+++ UserRegistration/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace UserRegistration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("--------------User Registration----------------");
+            User user = new User();
+            user.ValidateFirstName();
+        }
+    }
+}
